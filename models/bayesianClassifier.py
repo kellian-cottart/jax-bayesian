f0c6b7c9974140a5b75a3eacaa510e737ac61c0a
kellianoy <kellian.cottart@gmail.com>
2024-10-18 12:50:20 +0200

    feat: bayesian NN

--- models/bayesianClassifier.py
+++ models/bayesianClassifier.py
@@ -1,60 +1,112 @@
-from equinox.nn import Linear
-from equinox import Module
-from jax.random import split, normal
+from equinox import Module, field, filter_jit
+from equinox import _misc
+from jax.random import split, normal, uniform
 from jax.nn import relu
-from jax.numpy import reshape, ndarray
+from jax.numpy import reshape, ones
+from jax import jit, vmap
+from jax.numpy import dot, shape, broadcast_to, einsum
+from typing import Literal, Optional, Union
+from jaxtyping import PRNGKeyArray, Array
+from math import sqrt
+from functools import partial
 
 
-class BayesianLinear(Module):
-    """ Bayesian Linear Layer
+class BayesianLinear(Module, strict=True):
+    """Performs a linear transformation."""
 
-    Weights have two parameters: mean and variance.
-    They are sampled using the reparameterization trick w = mean + std * eps, where eps ~ N(0, 1)
+    weight_mu: Array
+    weight_sigma: Array
+    bias_mu: Optional[Array]
+    bias_sigma: Optional[Array]
+    in_features: Union[int, Literal["scalar"]] = field(static=True)
+    out_features: Union[int, Literal["scalar"]] = field(static=True)
+    use_bias: bool = field(static=True)
 
-    Args:
-        in_features: int, number of input features
-        out_features: int, number of output features
-        use_bias: bool, whether to use bias
-        key: PRNGKey, random key
-
-    """
-    weight: ndarray
-    bias: ndarray
-
-    def __init__(self, in_features, out_features, use_bias, key):
-        self.weight.mu = normal(key, (out_features, in_features))
-        self.weight.sigma = normal(key, (out_features, in_features))
-        self.bias.mu = normal(key, (out_features,)) if use_bias else None
-        self.bias.sigma = normal(key, (out_features,)) if use_bias else None
-
-    def __call__(self, x, samples, key):
-        """ Forward pass sampled `samples` times
+    def __init__(
+        self,
+        in_features: Union[int, Literal["scalar"]],
+        out_features: Union[int, Literal["scalar"]],
+        use_bias: bool = True,
+        dtype=None,
+        *,
+        key: PRNGKeyArray,
+        sigma_init: float = 0.1,
+    ):
+        """ Initialises the Bayesian Linear Layer
 
         Args:
-            x: , input tensor
-            samples: int, number of samples to take
+            in_features: The input size. The input to the layer should be a vector of
+                shape `(in_features,)`
+            out_features: The output size. The output from the layer will be a vector
+                of shape `(out_features,)`.
+            use_bias: Whether to add on a bias as well.
+            dtype: The dtype to use for the weight and the bias in this layer.
+                Defaults to either `jax.numpy.float32` or `jax.numpy.float64` depending
+                on whether JAX is in 64-bit mode.
+            key: A `jax.random.PRNGKey` used to provide randomness for parameter
+                initialisation. (Keyword only argument.)
         """
-        if samples == 0:
-            return x @ self.weight.mu.T + self.bias.mu
-        else:
-            weight = self.weight.mu + self.weight.sigma * \
-                normal(key, (samples, *self.weight.mu.shape))
-            bias = self.bias.mu + self.bias.sigma * \
-                normal(key, (samples, *self.bias.mu.shape))
-            return x @ weight.T + bias
+        dtype = _misc.default_floating_dtype() if dtype is None else dtype
+        wkey, bkey = split(key, 2)
+        in_features_ = 1 if in_features == "scalar" else in_features
+        out_features_ = 1 if out_features == "scalar" else out_features
+        lim = 1 / sqrt(in_features_)
+        wshape = (out_features_, in_features_)
+        # initalizing mu as 1/sqrt(in_features) uniformly
+        self.weight_mu = uniform(wkey, wshape, minval=-lim, maxval=lim)
+        self.weight_sigma = ones(wshape, dtype)*sigma_init
+        bshape = (out_features_,)
+        self.bias_mu = uniform(bkey, bshape, minval=-lim,
+                               maxval=lim) if use_bias else None
+        self.bias_sigma = ones(bshape, dtype)*sigma_init if use_bias else None
+        self.in_features = in_features
+        self.out_features = out_features
+        self.use_bias = use_bias
+
+    def __call__(self, x: Array, samples: int, *, key: PRNGKeyArray) -> Array:
+        """ Call function for bayesian linear layer
+        `samples` forward passes using weights reparametrization w = mu + sigma * epsilon, epsilon ~ N(0, 1)
+
+        Args:
+            x: input tensor
+            samples: number of samples
+            rng: random key
+        """
+        # we need to do
+        # broadcast x if needed, weights and sigma
+        if len(shape(x)) == 1:
+            # repeat the content of x samples times
+            x = broadcast_to(x, (samples, *shape(x)))
+        mu = broadcast_to(self.weight_mu, (samples, *shape(self.weight_mu)))
+        sigma = broadcast_to(
+            self.weight_sigma, (samples, *shape(self.weight_sigma)))
+        weights = mu + sigma * normal(key, shape(mu))
+
+        output = vmap(lambda w, x: dot(w, x), in_axes=(0, 0))(weights, x)
+        if self.use_bias:
+            biases = vmap(sample_fn, in_axes=(0, 0, 0))(
+                self.bias_mu, self.bias_sigma, split(key, samples))
+            output = output + biases
+        return output
+
+
+def sample_fn(mu, sigma, key):
+    return mu + sigma * normal(key, shape(mu))
 
 
 class SmallBayesianNetwork(Module):
-    linear1: Linear
-    linear2: Linear
+    linear1: BayesianLinear
+    linear2: BayesianLinear
 
-    def __init__(self, key):
+    def __init__(self, key, sigma_init):
         key1, key2 = split(key)
-        self.linear1 = BayesianLinear(784, 50, use_bias=False, key=key1)
-        self.linear2 = BayesianLinear(50, 10, use_bias=False, key=key2)
+        self.linear1 = BayesianLinear(
+            784, 50, use_bias=False, key=key1, sigma_init=sigma_init)
+        self.linear2 = BayesianLinear(
+            50, 10, use_bias=False, key=key2, sigma_init=sigma_init)
 
-    def __call__(self, x):
+    def __call__(self, x, samples, key):
         x = reshape(x, (-1))
-        x = relu(self.linear1(x))
-        x = self.linear2(x)
+        x = relu(self.linear1(x, samples=samples, key=key))
+        x = self.linear2(x, samples=samples, key=key)
         return x
--- permutedmnist-test.py
+++ permutedmnist-test.py
@@ -8,40 +8,52 @@ from models import *
 from datetime import datetime
 import os
 import json
+import functools as ft
+from shutil import rmtree
 
 
-@jax.jit
-def loss_fn(model, images, labels, perm=None):
+@ft.partial(jax.jit, static_argnames=("samples"))
+def loss_fn(model, images, labels, perm=None, samples=None, rng=None):
     if perm is not None:
         images = images.reshape(
             images.shape[0], -1)[:, perm].reshape(images.shape)
-    output = jax.vmap(model)(images)
+    if samples is not None:
+        key, rng = jax.random.split(rng)
+        output = jax.vmap(model, in_axes=(0, None, None))(
+            images, samples, key).mean(axis=1)
+    else:
+        output = jax.vmap(model)(images)
     loss = -jnp.sum(jax.nn.log_softmax(output) * labels)
     return loss
 
 
-@jax.jit
-def train_fn(carry, data):
-    model, opt_state, perm = carry
+@ft.partial(jax.jit, static_argnames=("samples"))
+def train_fn(carry, data, samples=None):
+    model, opt_state, perm, rng = carry
     images, labels = data
     # Compute the loss
-    loss, grads = jax.value_and_grad(loss_fn)(model, images, labels, perm)
+    loss, params = jax.value_and_grad(loss_fn)(
+        model, images, labels, perm, samples, rng)
     # Update
-    updates, opt_state = optimizer.update(grads, opt_state)
+    updates, opt_state = optimizer.update(params, opt_state)
     model = optax.apply_updates(model, updates)
-    return (model, opt_state, perm), loss
+    return (model, opt_state, perm, rng), loss
 
 
-@jax.jit
-def test_fn(model, images, labels):
-    output = jax.vmap(model)(images)
+@ft.partial(jax.jit, static_argnames=("samples"))
+def test_fn(model, images, labels, samples=None, rng=None):
+    if samples is not None:
+        output = jax.vmap(model, in_axes=(0, None, None))(
+            images, samples, rng).mean(axis=1)
+    else:
+        output = jax.vmap(model)(images)
     pred = jnp.argmax(output, axis=-1)
     labels = jnp.argmax(labels, axis=-1)
     accuracy = jnp.mean(pred == labels)
     return accuracy
 
 
-@jax.jit
+@ eqx.filter_jit
 def test_batch_permutation_fn(model, image_batch, label_batch, batched_permutations):
     accuracies = jnp.zeros(len(batched_permutations))
     for i, perm in enumerate(batched_permutations):
@@ -52,10 +64,9 @@ def test_batch_permutation_fn(model, image_batch, label_batch, batched_permutati
     return accuracies
 
 
-@jax.jit
-def permute_and_test(model, permutations, image_batch, label_batch):
+@ eqx.filter_jit
+def permute_and_test(model, permutations, image_batch, label_batch, max_perm_parallel=25):
     """ We can't fit everything in one GPU when using too many permutations, so we must split permutations into batches """
-    max_perm_parallel = 25
     batched_permutations = jnp.array(
         jnp.split(permutations, len(permutations) // max_perm_parallel)) if len(permutations) > max_perm_parallel else jnp.array([permutations])
     accuracies = jax.vmap(test_batch_permutation_fn, in_axes=(None, None, None, 0))(
@@ -75,20 +86,71 @@ def prepare_data(data, targets, batch_size):
     return data.reshape(-1, batch_size, *data.shape[1:]), targets.reshape(-1, batch_size, num_classes)
 
 
-if __name__ == "__main__":
+def configure_networks(configuration, rng):
 
+    # make a dictionary of maps
+    select_network = {
+        "bayesianmlp": SmallBayesianNetwork,
+        "mlp": SmallNetwork,
+    }
+    if not "network_params" in configuration:
+        raise ValueError("Network parameters not found")
+    try:
+        model = select_network[configuration["network"]](
+            key=rng, **configuration["network_params"])
+    except KeyError as e:
+        raise KeyError("Error with provided keys: ", e)
+
+    return model
+
+
+def configure_optimizer(configuration, model):
+    select_optimizer = {
+        "sgd": optax.sgd,
+        "adam": optax.adam,
+    }
+    if not "optimizer_params" in configuration:
+        raise ValueError("Optimizer parameters not found")
+    try:
+        optimizer = select_optimizer[configuration["optimizer"]](
+            **configuration["optimizer_params"]
+        )
+    except KeyError as e:
+        raise KeyError("Error with provided keys: ", e)
+
+    opt_state = optimizer.init(eqx.filter(model, eqx.is_inexact_array))
+    return optimizer, opt_state
+
+
+if __name__ == "__main__":
     N_ITERATIONS = 1
     configurations = [
         {
+            "network": "bayesianmlp",
+            "network_params":   {
+                "sigma_init": 0.1
+            },
+            "optimizer": "sgd",
+            "optimizer_params": {
+                "learning_rate": 0.001
+            },
             "task": "MNIST",
+            "n_train_samples": 1,
+            "n_test_samples": 1,
             "n_tasks": 1,
             "epochs": 100,
             "train_batch_size": 1,
             "test_batch_size": 128,
-            "seed": 1000 + i
+            "seed": 1000 + i,
+            "max_perm_parallel": 25,
+            "sigma_init": 0.1,
         } for i in range(N_ITERATIONS)
     ]
-
+    # convert all fields to lowercase if string
+    for config in configurations:
+        config = {k: v.lower() if isinstance(
+            v, str) else v for k, v in config.items()}
+    # create a timestamp
     TIMESTAMP = datetime.now().strftime("%Y%m%d-%H%M%S-")
     for k, configuration in enumerate(configurations):
         FOLDER = TIMESTAMP + \
@@ -106,49 +168,58 @@ if __name__ == "__main__":
         # save config
         with open(SAVE_PATH + "/config.json", "w") as f:
             json.dump(configuration, f, indent=4)
-        # Load the MNIST dataset
-        loader = GPULoading()
-        train, test, shape, num_classes = loader.task_selection(
-            configuration["task"])
-        # Initialize the model
-        rng = jax.random.PRNGKey(configuration["seed"])
-        # Permutations
-        if configuration["task"] == "PermutedMNIST":
-            permutations = jnp.array(
-                [jax.random.permutation(key, jnp.array(shape).prod()) for key in jax.random.split(rng, configuration["n_tasks"])])
-        model = SmallNetwork(rng)
-        # Define the optimizer
-        optimizer = optax.sgd(learning_rate=0.001)
-        opt_state = optimizer.init(eqx.filter(model, eqx.is_inexact_array))
-        # Prepare datasets
-        task_train_images, task_train_labels = prepare_data(
-            train.data, train.targets, configuration["train_batch_size"])
-        test_train_images, test_train_labels = prepare_data(
-            test.data, test.targets, configuration["test_batch_size"])
 
-        for task in tqdm(range(configuration["n_tasks"]), desc="Tasks"):
-            tqdm.write(f"Task {task+1}/{configuration['n_tasks']}")
-            # Train the model
-            for epoch in tqdm(range(configuration["epochs"]), desc="Epochs"):
-                special_perm = permutations[task] if configuration["task"] == "PermutedMNIST" else None
-                (model, opt_state, _), loss = jax.lax.scan(f=train_fn, init=(
-                    model, opt_state, special_perm), xs=(task_train_images, task_train_labels))
-                # Test the model and compute accuracy
-                if epoch % 1 == 0:
-                    if configuration["task"] == "PermutedMNIST":
-                        accuracies = jnp.zeros(configuration["n_tasks"])
-                        accuracies = jax.vmap(permute_and_test, in_axes=(None, None, 0, 0))(
-                            model, permutations, test_train_images, test_train_labels).mean(axis=0)
-                    else:
-                        accuracies = jnp.mean(jax.vmap(test_fn, in_axes=(None, 0, 0))(
-                            model, test_train_images, test_train_labels))
-                        accuracies = jnp.array([accuracies])
-                    for i, acc in enumerate(accuracies):
-                        # print ten task acc max per line using \t
-                        tqdm.write(f"t{i}:{acc.item()*100:.2f}%", end="\t" if i % 10 !=
-                                   9 and i != len(accuracies) - 1 else "\n")
-                    tqdm.write(
-                        f"Epoch {epoch+1}/{configuration['epochs']} - Loss: {loss.mean():.2f}")
-                    # save accuracy as jax array
-                    with open(os.path.join(DATA_PATH, f"task{task}-epoch{epoch}.npy"), "wb") as f:
-                        jnp.save(f, accuracies)
+        try:
+            # Load the MNIST dataset
+            loader = GPULoading()
+            train, test, shape, num_classes = loader.task_selection(
+                configuration["task"])
+            # Initialize the model
+            rng = jax.random.PRNGKey(configuration["seed"])
+            # Permutations
+            if configuration["task"] == "PermutedMNIST":
+                permutations = jnp.array(
+                    [jax.random.permutation(key, jnp.array(shape).prod()) for key in jax.random.split(rng, configuration["n_tasks"])])
+
+            model = configure_networks(configuration, rng)
+            optimizer, opt_state = configure_optimizer(configuration, model)
+            # Prepare datasets
+            task_train_images, task_train_labels = prepare_data(
+                train.data, train.targets, configuration["train_batch_size"])
+            test_train_images, test_train_labels = prepare_data(
+                test.data, test.targets, configuration["test_batch_size"])
+
+            train_samples = configuration["n_train_samples"] if "n_train_samples" in configuration else None
+            test_samples = configuration["n_test_samples"] if "n_test_samples" in configuration else None
+
+            for task in tqdm(range(configuration["n_tasks"]), desc="Tasks"):
+                tqdm.write(f"Task {task+1}/{configuration['n_tasks']}")
+                # Train the model
+                for epoch in tqdm(range(configuration["epochs"]), desc="Epochs"):
+
+                    special_perm = permutations[task] if configuration["task"] == "PermutedMNIST" else None
+                    (model, opt_state, _, _), loss = jax.lax.scan(f=ft.partial(train_fn, samples=train_samples), init=(
+                        model, opt_state, special_perm, rng), xs=(task_train_images, task_train_labels))
+                    # Test the model and compute accuracy
+                    if epoch % 1 == 0:
+                        if configuration["task"] == "PermutedMNIST":
+                            accuracies = jnp.zeros(configuration["n_tasks"])
+                            accuracies = jax.vmap(permute_and_test, in_axes=(None, None, 0, 0, None))(
+                                model, permutations, test_train_images, test_train_labels, configuration["max_perm_parallel"]).mean(dim=0)
+                        else:
+                            accuracies = jnp.mean(jax.vmap(test_fn, in_axes=(None, 0, 0, None, None))(
+                                model, test_train_images, test_train_labels, test_samples, rng))
+                            accuracies = jnp.array([accuracies])
+                        for i, acc in enumerate(accuracies):
+                            tqdm.write(f"{acc.item()*100:.2f}%", end="\t" if i % 10 !=
+                                       9 and i != len(accuracies) - 1 else "\n")
+                        tqdm.write(
+                            f"Epoch {epoch+1}/{configuration['epochs']} - Loss: {loss.mean():.2f}")
+                        # Save accuracy as jax array
+                        with open(os.path.join(DATA_PATH, f"task{task}-epoch{epoch}.npy"), "wb") as f:
+                            jnp.save(f, accuracies)
+        except KeyboardInterrupt as e:
+            rmtree(SAVE_PATH)
+        except Exception as e:
+            rmtree(SAVE_PATH)
+            raise e
